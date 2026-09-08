f7dd742b3b0a110596325f3b10025d2b2e118ac1
malek <malek2062002@mail.com>
2026-09-09 01:38:21 +0300

    uptade socet with api link

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io("https://real-estate-nest-production.up.railway.app", {
 
    autoConnect: false,
 });
